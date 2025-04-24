b10afef964934cd7716b1d59c35c818efbb5125a
Petr <stone.ptr@gmail.com>
2025-04-24 23:21:22 +0400

    Домашнее задание по Compose №2 Кастомный Layout, который будет работать как Grid

--- app/src/main/java/ru/otus/compose/customlayout/ComposeHomeworkApp.kt
+++ app/src/main/java/ru/otus/compose/customlayout/ComposeHomeworkApp.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,56 +19,28 @@ fun ComposeHomeworkApp() {
         columns = 3,
         modifier = Modifier.systemBarsPadding()
             .padding(14.dp)
+            .verticalScroll(rememberScrollState())
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(110.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(90.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(120.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(80.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(120.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.catanddot),
-            contentDescription = null,
-            Modifier.size(90.dp).padding(4.dp).border(2.dp, color = Color.Black)
-        )
+        CatAndDot(Modifier.size(100.dp))
+        CatAndDot(Modifier.size(110.dp))
+        CatAndDot(Modifier.size(90.dp))
+        CatAndDot(Modifier.size(120.dp))
+        CatAndDot(Modifier.size(100.dp))
+        CatAndDot(Modifier.size(80.dp))
+        CatAndDot(Modifier.size(100.dp))
+        CatAndDot(Modifier.size(120.dp))
+        CatAndDot(Modifier.size(100.dp))
+        CatAndDot(Modifier.size(90.dp))
     }
+}
+
+@Composable
+fun CatAndDot(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.catanddot),
+        contentDescription = null,
+        modifier = modifier
+            .padding(4.dp)
+            .border(2.dp, color = Color.Black)
+    )
 }
--- app/src/main/java/ru/otus/compose/customlayout/CustomLayoutHW.kt
+++ app/src/main/java/ru/otus/compose/customlayout/CustomLayoutHW.kt
@@ -1,17 +1,17 @@
 package ru.otus.compose.customlayout
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import kotlin.math.max
 
 /**
  * Task: Make custom grid layout
@@ -22,11 +22,48 @@ fun CustomLayoutHW(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit = { }
 ) {
-    // Replace Column with your custom layout
-    Column(
-        modifier = modifier,
-    ) {
-        content()
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        val constraintColumnMaxWidth = constraints.maxWidth / columns
+        val placeables = measurables.map { measurable ->
+            measurable.measure(
+                Constraints(
+                    maxWidth = constraintColumnMaxWidth,
+                    maxHeight = constraints.maxHeight,
+                )
+            )
+        }
+        val maxWs = Array<Int>(size = columns) { 0 }
+        val maxHs = Array<Int>(size = measurables.size / columns + 1) { 0 }
+        placeables.forEachIndexed { index, placeable ->
+            val column = index % columns
+            maxWs[column] = max(maxWs[column], placeable.width)
+
+
+            val row = index / columns
+            maxHs[row] = max(maxHs[row], placeable.height)
+        }
+
+        layout(
+            maxWs.fold(0) { acc, n -> acc + n },
+            maxHs.fold(0) { acc, n -> acc + n },
+        ) {
+            var x = 0
+            var y = 0
+
+            placeables.forEachIndexed { index, placeable ->
+                placeable.place(x, y)
+                val isNewRow = ((index + 1) % columns) == 0
+                if (isNewRow) {
+                    x = 0
+                    y += maxHs[index / columns]
+                } else {
+                    x += maxWs[index % columns]
+                }
+            }
+        }
     }
 }
 
@@ -41,56 +78,16 @@ fun CustomLayoutHWPreview() {
                 .border(2.dp, color = Color.Black)
                 .padding(4.dp)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(110.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(90.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(120.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(80.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(120.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(100.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.catanddot),
-                contentDescription = null,
-                Modifier.size(90.dp).padding(4.dp).border(2.dp, color = Color.Black)
-            )
+            CatAndDot(Modifier.size(100.dp))
+            CatAndDot(Modifier.size(110.dp))
+            CatAndDot(Modifier.size(90.dp))
+            CatAndDot(Modifier.size(120.dp))
+            CatAndDot(Modifier.size(100.dp))
+            CatAndDot(Modifier.size(80.dp))
+            CatAndDot(Modifier.size(100.dp))
+            CatAndDot(Modifier.size(120.dp))
+            CatAndDot(Modifier.size(100.dp))
+            CatAndDot(Modifier.size(90.dp))
         }
     }
 }
